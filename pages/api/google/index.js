6ecfd2eab4e570e62d8e81c72c07eb6dd0bcc76b
Mario <m.godinez@unexpect.mx>
2022-03-12 19:04:51 -0600

    finding bug

--- pages/api/google/index.js
+++ pages/api/google/index.js
@@ -5,8 +5,11 @@ import "../../../lib/passport";
 
 export default async function (req, res, next) {
   await dbConnect();
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-    session: false,
-  })(req, res, next);
+
+  res.status(200).json({ name: 'no hay 500 aqui' })
+
+  // passport.authenticate("google", {
+  //   scope: ["profile", "email"],
+  //   session: false,
+  // })(req, res, next);
 }

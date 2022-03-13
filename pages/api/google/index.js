8f164dd14c90bfe7b5e576250711b96e7704e2fd
Mario <m.godinez@unexpect.mx>
2022-03-12 19:46:58 -0600

    rested changes

--- pages/api/google/index.js
+++ pages/api/google/index.js
@@ -1,15 +1,15 @@
-// import passport from "passport";
-// import { dbConnect } from "../../../mongodb/dbConnect";
+import passport from "passport";
+import { dbConnect } from "../../../mongodb/dbConnect";
 
-// import "../../../lib/passport";
+import "../../../lib/passport";
 
 export default async function (req, res, next) {
-  // await dbConnect();
+  await dbConnect();
 
   res.status(200).json({ name: 'no hay 500 aqui' })
 
-  // passport.authenticate("google", {
-  //   scope: ["profile", "email"],
-  //   session: false,
-  // })(req, res, next);
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+    session: false,
+  })(req, res, next);
 }
